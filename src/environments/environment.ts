9b9fc36a1940f7cc00b35a7d8437a8281562b4ed
Josef Shainskiy <josef2011@gmail.com>
2018-01-11 14:04:35 +0200

    Add existing file

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebase: {
+        apiKey: "AIzaSyC_PdzHV5VB6ql5AjXRyuiVo-lWx-IzcrQ",
+        authDomain: "clientpanelapp-952cf.firebaseapp.com",
+        databaseURL: "https://clientpanelapp-952cf.firebaseio.com",
+        projectId: "clientpanelapp-952cf",
+        storageBucket: "clientpanelapp-952cf.appspot.com",
+        messagingSenderId: "787108850564"
+    }
 };
